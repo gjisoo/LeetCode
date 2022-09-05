65bffcf912b0dd38fd23e5d80acccbf158652580
gjisoo <103836040+gjisoo@users.noreply.github.com>
2022-09-05 21:36:11 +0900

    Time: 113 ms (14.28%), Space: 45 MB (65.14%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,23 +1,20 @@
-import java.util.*;
-
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         
         int[] answer = new int[2];
         
         for (int i = 0; i < nums.length-1; i++){
-            for (int j = 1; j < nums.length; j++){
-                if (nums[i] + nums[j] == target){
-                    if (i == j){
-                        continue;
-                    } else {
-                        answer[0] = i;
-                        answer[1] = j;
-                        break;
-                    }
+            for (int j = i+1; j < nums.length; j++) {
+                if (i == j){
+                    continue;
+                } else if (nums[i] + nums[j] == target) {
+                    answer[0] = i;
+                    answer[1] = j;
+                    break;
                 }
             }
         }
+        
         return answer;
     }
 }
