d0c912715a79c3f014b1271e3ce02cc9cacc5485
gjisoo <103836040+gjisoo@users.noreply.github.com>
2022-09-06 06:55:44 +0900

    Time: 15 ms (21.36%), Space: 47.5 MB (17.57%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.java
@@ -0,0 +1,37 @@
+import java.util.*;
+class Solution {
+    public int romanToInt(String s) {
+        
+        HashMap<Character,Integer> symbol = new HashMap<Character,Integer>(7);
+        // 기호가 의미하는 값 입력
+        symbol.put('I',1);
+        symbol.put('V',5);
+        symbol.put('X',10);
+        symbol.put('L',50);
+        symbol.put('C',100);
+        symbol.put('D',500);
+        symbol.put('M',1000);
+        
+        int sLength = s.length();
+        
+        Stack<Integer> value = new Stack<>();
+        value.push(symbol.get(s.charAt(0)));
+        
+        // 값을 스택에 쌓기
+        for(int i = 1; i < sLength; i++){
+            if(value.peek() < symbol.get(s.charAt(i))){
+                value.push(symbol.get(s.charAt(i)) - value.pop());
+            } else {
+                value.push(symbol.get(s.charAt(i)));
+            }
+        }
+        
+        int answer = 0;
+        while(!value.empty()){
+            answer += value.pop();
+        }
+        
+        
+        return answer;
+    }
+}
