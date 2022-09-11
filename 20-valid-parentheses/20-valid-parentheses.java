0c10651cb8ea0a6b21b6f488a45b924b348111c0
gjisoo <103836040+gjisoo@users.noreply.github.com>
2022-09-11 18:27:59 +0900

    Time: 4 ms (32.24%), Space: 42.1 MB (42.35%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -4,10 +4,13 @@ class Solution {
         Stack<Character> left = new Stack<>();
         
         for(int i = 0; i < s.length(); i++){
+            // 다음 기호로 계속 초기화
             char c = s.charAt(i);
+            // 왼쪽 괄호일 경우 stack에 저장
             if (c == '(' || c == '{' || c == '['){
                 left.push(c);
-            } else if (c == ')' && !left.isEmpty() && left.peek() == '('){
+            } // 오른쪽 괄호일때 stack이 비어있지 않고 stack에 마지막으로 저장된 괄호가 왼쪽 괄호일 경우
+            else if (c == ')' && !left.isEmpty() && left.peek() == '('){
                 left.pop();
             } else if (c == '}' && !left.isEmpty() && left.peek() == '{'){
                 left.pop();
